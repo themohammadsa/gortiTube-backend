2750e6a33f8e92d3db1e64dacf071476a647999d
themohammadsa <59393936+themohammadsa@users.noreply.github.com>
2021-05-13 18:13:47 +0530

    added feat: added single-use route for cors

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const mongoose = require("mongoose")
 const app = express();
-var cors = require('cors')
+const cors = require('cors')
 
 const VideoLibraryRouter = require("./Routes/VideoLibrary.route")
 const { addToDatabase } = require("./Database/addToDatabase.js")
 const { databaseConnection } = require("./Database/databaseConnection.connect.js")
 const PORT = 3000;
 
-app.use(cors());
-
+const corsOptions = {
+  origin: "https://gortitube.netlify.app"
+}
 
 databaseConnection();
 
+// Initial function run to add the data to MongoDB
 // addToDatabase();
 
-app.use("/", VideoLibraryRouter);
+app.use("/", cors(corsOptions), VideoLibraryRouter);
 
 
 
